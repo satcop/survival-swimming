cdada2dfae62d9fec49831f32ef4ccc2061e3f7f
박규태 <satcop@naver.com>
2026-09-19 19:44:07 +0900

    파일추가

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,6 +12,7 @@ module.exports = merge(common, {
     new CopyPlugin({
       patterns: [
         { from: 'img', to: 'img' },
+        { from: 'doc', to: 'doc' },
         { from: 'css', to: 'css' },
         { from: 'js/vendor', to: 'js/vendor' },
         { from: 'icon.svg', to: 'icon.svg' },
